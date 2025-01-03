1b2960b9fcfe0661716cc5bdac628683a2043874
omercikan <omercikan36@gmail.com>
2025-01-03 16:07:29 +0300

    Project configurations files ready

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'midnight': '#161880',
+    },
+    extend: {},
+  },
+  plugins: [],
+} 
