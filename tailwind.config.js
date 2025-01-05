0ed3b1debec5eb165334c0164c0f956a562026de
omercikan <omercikan36@gmail.com>
2025-01-05 15:38:42 +0300

    Material UI has been included and Tailwind container settings have been configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      'midnight': '#161880',
+    container: {
+      padding: {
+        DEFAULT: '0rem',
+        sm: '2rem',
+        lg: '1rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
     },
     extend: {},
   },
